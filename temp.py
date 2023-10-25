c1a07234f806052a435e18c0f04e00028022878d
Алексей С <76011034+sem-a@users.noreply.github.com>
2023-10-25 20:41:23 +0300

    Add files via upload

--- temp.py
+++ temp.py
@@ -1,152 +1,127 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Apr 30 21:24:04 2023
-
-@author: aleks
-"""
-
 import numpy as np
-import time
-import random as r
-import math as m
 import matplotlib.pyplot as plt
+import time as t
+import random as r
+import numba as nb
 from progress.bar import IncrementalBar
 
-
-start_time = time.perf_counter()
-matrix_size =  int(input('Введите размер матрицы: '))
+start_time = t.perf_counter()
+matrix_size = 100#int(input('Введите размер матрицы: '))
 coord_point_array = []
+repeat_points = []
 
+@nb.jit
 def func_1(x, y):
     """ функция 1 """
     return -x*(x**2 - 5) - y
+@nb.jit
 def func_2(x, y):
     """ функция 2 """
     return x
 
-def graficSystem(X, Y):
-    """ график системы """
+def calc_start_point(quantity, segment):
+    """ Расчет стартовой точки: количество, массив отрезка по Х и по У """
+    start_point = [ [r.uniform(segment[0][0], segment[0][1]), r.uniform(segment[1][0], segment[1][1])] for row in range(quantity)]
+    return start_point
+
+def draw_grafic(X, Y):
+    """ Построить график системы """
     plt.plot(X, Y)
 
-def calculation_cell_size(X, Y, matrix_size):
-    """ рассчет размера клетки """
-    cell_size = [((np.max(X) - np.min(X)) / (matrix_size - 5)), ((np.max(Y) - np.min(Y)) / (matrix_size - 5))]
+def calc_cell_size(X, Y,  matrix_size):
+    """ Рассчет размера клетки: массив точек по Х, массив точек по У, размер матрицы """
+    cell_size = [((np.max(X) - np.min(X)) / (matrix_size - 1)), ((np.max(Y) - np.min(Y)) / (matrix_size - 1))]
     return np.max(cell_size)
 
 def error_len(x, y, grid):
-    """ ошибка размера матрицы """
+    """ Ошибка размера матрицы """
     if(x > (len(grid) - 1) or y > (len(grid) - 1) or x < 0 or y < 0):
-        #print('Ошибка: Слишком маленький размер матрицы! Увеличьте размер матрицы')
-        return False
-    else:
-        return True
+        return False # если x или у 
+    else:            # вышли за границы матрицы 
+        return True  # вывести ошибку, иначе продолжить
 
 def save_coord_point(coord_point):
-    """ сохранение точек матрицы в массив """
+    """ Сохранение точек матрицы в массив: массив координат """
     coord_point_array.append(coord_point)
 
 
 def comparison_point(arr, grid):
-    """ принимает: массив, матрицу """
-    for i in range(len(arr)-1):
-        if(grid[arr[i][1]][arr[i][0]] == 0):
-            grid[arr[i][1]][arr[i][0]] = [0, 0, 0, 0]
-    """ заполнение массива точками """
-    for i in range(1, len(arr)-1):
+    """ Заполнение точками: массив с точками, матрица """
+    for i in range(1, len(arr) - 1):
         if(arr[i-1][1] < arr[i][1]):
             k = 0
-        if(arr[i-1][1] > arr[i][1]):
+        elif(arr[i-1][1] > arr[i][1]):
             k = 2
         if(arr[i-1][0] < arr[i][0]):
             k = 3
-        if(arr[i-1][0] > arr[i][0]):
+        elif(arr[i-1][0] > arr[i][0]):
             k = 1
         if(arr[i+1][1] < arr[i][1]):
             grid[arr[i][1]][arr[i][0]][k] = 1
-        if(arr[i+1][1] > arr[i][1]):
+            # if(grid[arr[i][1]][arr[i][0]][k] == 0 or  grid[arr[i][1]][arr[i][0]][k] == 1):
+            #     grid[arr[i][1]][arr[i][0]][k] = 1
+            # else:
+            #     repeat_point = grid[arr[i][1]][arr[i][0]]
+            #     print(repeat_point)
+            #     repeat_point[k] = 1
+            #     repeat_points.append(repeat_point)
+        elif(arr[i+1][1] > arr[i][1]):
             grid[arr[i][1]][arr[i][0]][k] = 3
         if(arr[i+1][0] < arr[i][0]):
             grid[arr[i][1]][arr[i][0]][k] = 4
-        if(arr[i+1][0] > arr[i][0]):
+        elif(arr[i+1][0] > arr[i][0]):
             grid[arr[i][1]][arr[i][0]][k] = 2
 
-def check_arr(arr, grid):
-    """ Принимает: массив с точками, матрицу """
-    for i in range(len(arr)-1):
-        grid[arr[i][1]][arr[i][0]] = 1
-
-
-def calc_start_point(quantity, segment):
-    """ Рассчет стартовой точки """
-    start_point = [[None, None]] * quantity
-    for i in range(quantity):
-        start_point[i] = [r.uniform(segment[0][0], segment[0][1]), r.uniform(segment[1][0], segment[1][1])]
-    return start_point
-
 def create_grid(matrix_size, func_1, func_2, cell_size=0):
-    """ Принимает: размер матрицы, размер клетки, функция 1, функция 2, размер клетки(необязательно) """
-
-    # Начальные условия:
-    segment = [0,10]
-    h = 0.001
-    n = int( (segment[1] - segment[0]) / h )
-    quantity_start_point = int(input('Количество стартовых точек: '))
-    start_point = calc_start_point(quantity_start_point, [[-2.6, 2.6], [-4.3, 4.3,]])
+    """ Принимает: размер матрицы, функция 1, функция 2, размер клетки(необязательно) """
+    """ Начальные условия """
+    segment = [0, 10]
+    h = 0.01
+    n = int((segment[1] - segment[0]) / h)
+    quantity_start_point = 100#int(input('Количество стартовых точек: '))
+    start_point = calc_start_point(quantity_start_point, [[-2.6, 2.6], [-4.3, 4.3]])
     mu = 0.1
-    progress_bar = IncrementalBar('Str', max = quantity_start_point)
-
+    #progress_bar = IncrementalBar('Start Point', max = quantity_start_point)
     try:
-        grid = [[[0, 0, 0, 0] for col in range(matrix_size)] for row in range(matrix_size)] # заполнение сетки нулями
-
-        # Заполнение массивов X, Y
+        grid = [[[0, 0, 0, 0] for col in range(matrix_size)] for row in range(matrix_size)] # заполнение матрицы нулями
         for j in range(len(start_point)):
-            point = [[], []] # массив содержащий [X, Y]
+            point = [[], []] # массив содержащий решения X и Y
             for i in range(n):
                 if(i == 0):
                     point[0].append(start_point[j][0] + func_1(start_point[j][0], start_point[j][1]) * h / mu)
                     point[1].append(start_point[j][1] + func_1(start_point[j][0], start_point[j][1]) * h / mu)
                 point[0].append(point[0][i] + func_1(point[0][i], point[1][i]) * h / mu)
                 point[1].append(point[1][i] + func_2(point[0][i], point[1][i]) * h)
-
-            graficSystem(point[0], point[1])
-
+            draw_grafic(point[0], point[1])
             if(cell_size == 0):
-                cell_size = calculation_cell_size(point[0], point[1], matrix_size) # размер клетки
-                print('Размер клетки:', cell_size)
-
-
-            x_temp = int( (matrix_size - 1) / 2 ) + int( point[0][0] / cell_size ) # Координата X первой точки
-            y_temp = int( (matrix_size - 1) / 2 ) + int( point[1][0] / cell_size ) # Координата У первой точки
-            # Заполнение матрицы по траектории движения
+                cell_size = calc_cell_size(point[0], point[1], matrix_size)
+            x_temp = int( (matrix_size - 1) / 2 ) + int( point[0][0] / cell_size ) # рассчет первых точек Х и У
+            y_temp = int( (matrix_size - 1) / 2 ) + int( point[0][0] / cell_size ) # относительно центра матрицы
+            """ Заполнение матрицы по траектории движения """
             for i in range(2, n):
                 if(error_len(x_temp, y_temp, grid)):
-                    x_coor = int( (matrix_size + 1) / 2 ) + int( point[0][i] / cell_size ) # Координата Х следующей точки
-                    y_coor = int( (matrix_size + 1) / 2 ) - int( point[1][i] / cell_size ) # Координата У следующей точки
+                    x_coor = int( (matrix_size + 1) / 2 ) + int( point[0][i] / cell_size ) # следующие координаты 
+                    y_coor = int( (matrix_size + 1) / 2 ) - int( point[1][i] / cell_size ) # X и Y
 
                     if(error_len(x_coor, y_coor, grid)):
                         if([x_temp, y_temp] != [x_coor, y_coor]):
-                            save_coord_point([x_temp, y_temp]) # Сохранение точек в массив
-                    else:
+                            save_coord_point([x_temp, y_temp])
+                    else: 
                         continue
-
-                    x_temp = x_coor # Переприсваиваем Х
-                    y_temp = y_coor # Переприсваиваем У
-                else:
+                    x_temp = x_coor # переприсваем точки х и у
+                    y_temp = y_coor # для дальнейших операций в цикле
+                else: 
                     continue
 
-            #check_arr(coord_point_array, grid)
             comparison_point(coord_point_array, grid)
-            progress_bar.next()
-
-        progress_bar.finish()
+            #progress_bar.next()            
+        #progress_bar.finish()
         return grid
     except:
-        print('Возникла непредвиденная ошибка!')
-
+        print('Ошибка!')
 matrix = create_grid(matrix_size, func_1, func_2)
 plt.savefig(f'{matrix_size}.png')
 my_file = open(f'{matrix_size}.txt', "w+")
 my_file.write(str(matrix))
 my_file.close()
-print('Время выполнения программы:', time.perf_counter() - start_time)
-plt.show()
+print('Время выполнения программы:', t.perf_counter() - start_time)
