a078a7cca67e7634e86a681fd41335466758339a
Алексей С <76011034+sem-a@users.noreply.github.com>
2024-03-26 19:28:48 +0300

    Add files via upload

--- temp.py
+++ temp.py
@@ -1,155 +1,253 @@
 import numpy as np
+import numba as nb
 import matplotlib.pyplot as plt
 import time as t
-import numba as nb
-import copy
-from progress.bar import IncrementalBar
-from numba.typed import List
 
-start_time = t.perf_counter()
-matrix_size = 100#int(input('Введите размер матрицы: '))
-coord_point_array = []
-repeat_points = []
+start_time = t.time()
+
+def draw_grid_for_grafic(cell_size, start_point_polygon):
+    running = True
+    i = 0
+    while running:
+        line_x = (start_point_polygon[0, 0] - cell_size) + i * cell_size
+        line_y = (start_point_polygon[1, 1] + cell_size) - i * cell_size
+        i += 1
+        if line_x < start_point_polygon[0, 1]:
+            plt.plot([line_x, line_x], [start_point_polygon[1, 0] - 1, start_point_polygon[1, 1] + 1], '-', linewidth=0.2, color='grey')
+        if line_y > start_point_polygon[1, 0]:
+            plt.plot([start_point_polygon[0, 0] - 1, start_point_polygon[0, 1] + 1], [line_y, line_y], '-', linewidth=0.2, color="grey")
+        if line_x > start_point_polygon[0, 1] + 1 and line_y < start_point_polygon[1, 0] - 1:
+            running = True
+            break
+
+
+def draw_grafic(point):
+    X = point[0, 1:]
+    Y = point[1, 1:]
+    plt.plot(X, Y)
+
+def draw_matrix(coord_point_arr):
+    X = []
+    Y = []
+    for i in range(len(coord_point_arr)):
+        X.append(coord_point_arr[i][0])
+        Y.append(coord_point_arr[i][1])
+
+    plt.plot(X, Y)
 
 @nb.njit(cache=True)
-def func_1(x, y):
-    """ функция 1 """
-    return -x*(x**2 - 5) - y
+def f_1(x, y):
+  return -x*(x**2 - 5) - y
 @nb.njit(cache=True)
-def func_2(x, y):
-    """ функция 2 """
-    return x + 1.3
+def f_2(x, y):
+  return x
 
 @nb.njit(cache=True)
-def calc_start_point(quantity, segment):
-    """Numba-optimized function for calculating start points"""
-    start_point = np.empty((quantity, 2))
-    for row in nb.prange(quantity):
-        start_point[row, 0] = np.random.uniform(segment[0][0], segment[0][1])
-        start_point[row, 1] = np.random.uniform(segment[1][0], segment[1][1])
+def method_euler(start_point, n, h, mu):
+    """ Решение методом Эйлера """
+    point = np.zeros( (2, n+1) )
+    x, y = start_point[0], start_point[1]
+    point[0, 0], point[1, 0] = x, y
+    for i in range(n):
+        dx = f_1(x, y) * h / mu
+        dy = f_2(x, y) * h
+        x += dx
+        y += dy
+        point[0, i+1], point[1, i+1] = x, y
+    return point
+
+@nb.njit(cache=True)
+def calc_start_point(quantity, polygon):
+    """ Рассчет стартовых точек """
+    x_min, x_max = polygon[0][0], polygon[0][1]
+    y_min, y_max = polygon[1][0], polygon[1][1]
+    start_point = np.empty( (quantity, 2) )
+    for i in nb.prange(quantity):
+        start_point[i, 0] = np.random.uniform(x_min, x_max)
+        start_point[i, 1] = np.random.uniform(y_min, y_max)
     return start_point
 
-def draw_grafic(X, Y):
-    """Numba-optimized function for plotting the system"""
-    plt.plot(X, Y, linewidth=0.2)
-
-def draw_repeat_points(cell_size):
-    """Numba-optimized function for plotting repeat points"""
-    for el in repeat_points: 
-        x_1 = (el[1] - int(matrix_size / 2)) * cell_size
-        x = [x_1, x_1 + cell_size, x_1 + cell_size, x_1, x_1]
-        y_1 = -(el[0] - int(matrix_size / 2)) * cell_size
-        y = [y_1, y_1, y_1 + cell_size, y_1 + cell_size, y_1]
-        plt.plot(x, y, 'red', linewidth=0.1)
+@nb.njit(cache=True)
+def calc_cell_size(point, size):
+    """ Рассчет размера клетки: массив точек, размер матрицы """
+    x_min, x_max = np.min(point[0]), np.max(point[0])
+    y_min, y_max = np.min(point[1]), np.max(point[1])
+    cell_size_x = (x_max - x_min) / (size - 1)
+    cell_size_y = (y_max - y_min) / (size - 1)
+    return max(cell_size_x, cell_size_y)
 
 @nb.njit(cache=True)
-def calc_cell_size(X, Y,  matrix_size):
-    """ Рассчет размера клетки: массив точек по Х, массив точек по У, размер матрицы """
-    cell_size = np.array([((np.max(X) - np.min(X)) / (matrix_size - 1)), ((np.max(Y) - np.min(Y)) / (matrix_size - 1))])
-    return np.max(cell_size)
+def error_len(x, y, grid_size):
+    """ Ошибка длины массива """
+    return 0 <= x < grid_size and \
+           0 <= y < grid_size
 
-def error_len(x, y, grid):
-    return not (x > (len(grid) - 1) or y > (len(grid) - 1) or x < 0 or y < 0)
+@nb.njit(cache=True)
+def save_coord_point(coord_point_arr, grid_size, cell_size, point, n):
+    """ Преобразование точек в координаты матрицы """
+    prev_arr = np.array([[
+        int( (grid_size - 1) / 2 ) + int( point[0, 0] / cell_size ), # рассчет точек 
+        int( (grid_size - 1) / 2 ) + int( point[1, 0] / cell_size )  # относительно центра
+    ]])                                                              # матрицы
+    for i in nb.prange(1, n):
+        curr_x = int( (grid_size + 1) / 2 ) + int( point[0, i] / cell_size )
+        curr_y = int( (grid_size + 1) / 2 ) + int( point[1, i] / cell_size )
 
-def save_coord_point(coord_point):
-    """ Сохранение точек матрицы в массив: массив координат """
-    coord_point_array.append(coord_point)
-    return coord_point_array
+        if(error_len(prev_arr[0][0], prev_arr[0][1], grid_size) and error_len(curr_x, curr_y, grid_size)):
+            if prev_arr[0][0] != curr_x or prev_arr[0][1] != curr_y:
+                coord_point_arr = np.vstack((coord_point_arr, prev_arr.reshape(1, 2)))
+            prev_arr[0][0], prev_arr[0][1] = curr_x, curr_y
+    return coord_point_arr[1:]
 
-def update_grid_and_repeat_points(grid, point_coords, k, new_value):
-    """
-    Обновляет grid и repeat_points с новым значением для точки с координатами point_coords и индексом k.
-    """
-    y, x = point_coords
-    if grid[y][x][0][k] in {0, new_value}:
-        grid[y][x][0][k] = new_value
-        grid[y][x][1] += 1
+@nb.njit(cache=True)
+def is_in(e, arr):
+    len_arr = len(arr)
+    if(len_arr > 0):
+        for i in nb.prange(len_arr):
+            if e['y'] == arr[i]['y'] and e['x'] == arr[i]['x'] and \
+                np.all(e['curr_arr'] == arr[i]['curr_arr']) and np.all(e['edited_arr'] == arr[i]['edited_arr']):
+                return True
+    return False
+
+@nb.njit(cache=True)
+def numba_vstack(arr1, arr2):
+    combined_lenght = len(arr1) + len(arr2)
+    result = np.zeros((combined_lenght), dtype=dtype_for_repeat_points)
+    for i in nb.prange(len(arr1)):
+        result[i] = arr1[i]
+    for i in nb.prange(len(arr2)):
+        result[len(arr1) + i] = arr2[i]
+    return result
+
+@nb.njit(cache=True)
+def calc_middle_iteration(grid, grid_size):
+    for row in range(grid_size):
+        for col in range(grid_size):
+            if grid[row, col]['iteration'] != 0:
+                res = int(grid[row, col]['iteration'] / grid[row, col]['pathway'])
+                grid[row, col]['iteration'] = res
+@nb.njit(cache=True)
+def update_grid_and_repeat_points(grid, point_coords, k, new_value, repeat_points, repeat_points_temp, pathway_counter):
+    """ Обновляет grid и repeat_points с новым значением """
+    x, y = int(point_coords[0]), int(point_coords[1])
+    if grid[y, x]['array'][k] in {0, new_value}:
+        grid[y, x]['array'][k] = new_value
+        grid[y, x]['iteration'] += 1.
+        if grid[y, x]['pathway'] <= pathway_counter:
+            grid[y, x]['pathway'] += 1
     else:
-        point_temp = grid[y][x][0].copy()  # Используем NumPy для копирования
+        point_temp = np.copy(grid[y, x]['array'])
         point_temp[k] = new_value
-        if not any(np.array_equal(point_temp, rp[3]) for rp in repeat_points):
-            repeat_points.append([y, x, grid[y][x][0].copy(), point_temp])
+        is_unique = True
+        for rp in repeat_points:
+            if np.array_equal(point_temp, rp[3]):
+                is_unique = False
+                break
+        if is_unique:
+            repeat_points_temp[0]['y'], repeat_points_temp[0]['x'] = y, x
+            repeat_points_temp[0]['curr_arr'] = np.copy(grid[y, x]['array'])
+            repeat_points_temp[0]['edited_arr'] = point_temp
+            if not is_in(repeat_points_temp[0], repeat_points):
+                repeat_points = numba_vstack(repeat_points, repeat_points_temp)
     return repeat_points
 
-def comparison_point(arr, grid):
-    """Заполнение точками: массив с точками, матрица."""
+@nb.njit(cache=True)
+def comparison_point(arr, grid, repeat_points, repeat_points_temp, pathway_counter):
+    """ Заполнение точками: массив с точками, матрица """
     for i in nb.prange(1, len(arr) - 1):
         k = 0
-        if arr[i-1][1] < arr[i][1]:
-            k = 0
-        elif arr[i-1][1] > arr[i][1]:
-            k = 2
-        if arr[i-1][0] < arr[i][0]:
+        if arr[i-1, 0] < arr[i, 0]:
             k = 3
-        elif arr[i-1][0] > arr[i][0]:
+        elif arr[i-1, 0] > arr[i, 0]:
             k = 1
-        if arr[i+1][1] < arr[i][1]:
-            update_grid_and_repeat_points(grid, arr[i], k, 1)
-        elif arr[i+1][1] > arr[i][1]:
-            update_grid_and_repeat_points(grid, arr[i], k, 3)
-        if arr[i+1][0] < arr[i][0]:
-            update_grid_and_repeat_points(grid, arr[i], k, 4)
-        elif arr[i+1][0] > arr[i][0]:
-            update_grid_and_repeat_points(grid, arr[i], k, 2)
+        if arr[i-1, 1] < arr[i, 1]:
+            k = 2
+        elif arr[i-1, 1] > arr[i, 1]:
+            k = 0
+        if arr[i+1, 1] < arr[i, 1]:
+            repeat_points = update_grid_and_repeat_points(grid, arr[i], k, 3,repeat_points,
+                                                          repeat_points_temp, pathway_counter)
+        elif arr[i+1, 1] > arr[i, 1]:
+            repeat_points = update_grid_and_repeat_points(grid, arr[i], k, 1, repeat_points,
+                                                          repeat_points_temp, pathway_counter)
+        elif arr[i+1, 0] < arr[i, 0]:
+            repeat_points = update_grid_and_repeat_points(grid, arr[i], k, 4, repeat_points,
+                                                          repeat_points_temp, pathway_counter)
+        elif arr[i+1, 0] > arr[i, 0]:
+            repeat_points = update_grid_and_repeat_points(grid, arr[i], k, 2, repeat_points,
+                                                          repeat_points_temp, pathway_counter)
+    return repeat_points
 
-def create_grid(matrix_size, func_1, func_2, cell_size=0):
-    """ Принимает: размер матрицы, функция 1, функция 2, размер клетки(необязательно) """
-    """ Начальные условия """
-    segment = [0, 10]
-    h = 0.001
-    n = int((segment[1] - segment[0]) / h)
-    quantity_start_point = 100#int(input('Количество стартовых точек: '))
-    segment_start_point = np.array([[-2.6, 2.6], [-4.3, 4.3]])
-    start_point = calc_start_point(quantity_start_point, segment_start_point)
-    mu = 0.1
-    progress_bar = IncrementalBar('Start Point', max = quantity_start_point)
-    try:
-        grid = [[[[0, 0, 0, 0], 0] for col in nb.prange(matrix_size)] for row in nb.prange(matrix_size)] # заполнение матрицы нулями
-        for j in nb.prange(len(start_point)):
-            point = [[], []] # массив содержащий решения X и Y
-            for i in nb.prange(n):
-                if(i == 0):
-                    point[0].append(start_point[j][0] + func_1(start_point[j][0], start_point[j][1]) * h / mu)
-                    point[1].append(start_point[j][1] + func_1(start_point[j][0], start_point[j][1]) * h / mu)
-                point[0].append(point[0][i] + func_1(point[0][i], point[1][i]) * h / mu)
-                point[1].append(point[1][i] + func_2(point[0][i], point[1][i]) * h)
-            draw_grafic(point[0], point[1])
-            if(cell_size == 0):
-                nb_point = np.array(point)
-                cell_size = calc_cell_size(nb_point[0], nb_point[1], matrix_size)
-            x_temp = int( (matrix_size - 1) / 2 ) + int( point[0][0] / cell_size ) # рассчет первых точек Х и У
-            y_temp = int( (matrix_size - 1) / 2 ) + int( point[0][0] / cell_size ) # относительно центра матрицы
-            """ Заполнение матрицы по траектории движения """
-            for i in nb.prange(2, n):
-                if(error_len(x_temp, y_temp, grid)):
-                    x_coor = int( (matrix_size + 1) / 2 ) + int( point[0][i] / cell_size ) # следующие координаты 
-                    y_coor = int( (matrix_size + 1) / 2 ) - int( point[1][i] / cell_size ) # X и Y
+#@nb.njit(cache=True)
+def create_grid(grid, repeat_points, grid_size, repeat_points_temp, cell_size = 0):
+    """ Принимает: размер матрицы, функция 1, функция 2, размер клетки """
+    coord_point_arr = np.zeros( (0, 2) ) # массив для координат относительно матрицы
+    for i in nb.prange(len(start_point_arr)):
+        start_point = start_point_arr[i]
+        pathway_counter = i
+        point = method_euler(start_point, n, h, mu)
+        draw_grafic(point)
+        if cell_size == 0 and i == 0:
+            cell_size = calc_cell_size(point, grid_size)
+        coord_point_arr = save_coord_point(coord_point_arr, grid_size, 
+                                           cell_size, point, n)
+        repeat_points = comparison_point(coord_point_arr, grid, repeat_points, 
+                                         repeat_points_temp, pathway_counter)
+    calc_middle_iteration(grid, grid_size)
+    return grid, point, coord_point_arr, repeat_points, cell_size
 
-                    if(error_len(x_coor, y_coor, grid)):
-                        if([x_temp, y_temp] != [x_coor, y_coor]):
-                            save_coord_point([x_temp, y_temp])
-                    else: 
-                        continue
-                    x_temp = x_coor # переприсваем точки х и у
-                    y_temp = y_coor # для дальнейших операций в цикле
-                else: 
-                    continue
-            comparison_point(coord_point_array, grid)
-            progress_bar.next()
-        draw_repeat_points(cell_size)            
-        progress_bar.finish()
-        print("Размер клетки:", cell_size)
-        return grid
-    except ValueError:
-        print(str(ValueError))
-matrix = create_grid(matrix_size, func_1, func_2)
-plt.savefig(f'{matrix_size}.svg')
+""" Глобальные переменные """
+
+segment = np.array([0, 10])
+h = 0.001 # step
+mu = 0.1 # parameter
+n = int( (segment[1] - segment[0]) / h )
+quantity_start_point = 10000 # number of starting points
+start_point_polygon = np.array( [ [-2.6, 2.6], [-4.3, 4.3] ] ) # a segment for
+                                                               # selecting starting points
+start_point_arr = calc_start_point(quantity_start_point, start_point_polygon)
+
+matrix_size = 10
+
+"""***********************"""
+
+""" Создание матрицы в глобальной области """
+
+dtype_for_matrix = np.dtype( [
+    ('array', np.int16, (4)),
+    ('iteration', np.int16),
+    ('pathway', np.int16)
+] )
+grid = np.zeros((matrix_size, matrix_size), dtype=dtype_for_matrix)
+
+dtype_for_repeat_points = np.dtype( [
+    ('y', np.int16), ('x', np.int16),
+    ('curr_arr', np.int16, (4)),
+    ('edited_arr', np.int16, (4))
+] )
+repeat_points = np.zeros((0), dtype=dtype_for_repeat_points) # an array of points 
+                                                             # that change repeatedly
+repeat_points_temp = np.zeros((1), dtype=dtype_for_repeat_points)
+"""***************************************"""
+
+grid, point, coord_point_arr, repeat_points, cell_size = create_grid(grid, repeat_points, matrix_size, repeat_points_temp)
+
+
 my_file = open(f'{matrix_size}.txt', "w+")
-for string in matrix:
+for string in reversed(grid):
     my_file.write(str(string) + '\n')
 my_file.close()
-file_repeat = open(f'repeatOf{matrix_size}.txt', "w+")
-for string in repeat_points:
-    file_repeat.write(str(string) + "\n")
-#print(repeat_points)
-print('Время выполнения программы:', t.perf_counter() - start_time)
+
+my_file = open(f'repeat_of_{matrix_size}.txt', "w+")
+for string in reversed(repeat_points):
+    my_file.write(str(string) + '\n')
+my_file.close()
+
+end_time = t.time()
+elapsed_time = end_time - start_time
+print(f"Время выполнения программы: {elapsed_time} секунд")
+print(f"Размер клетки: {cell_size}")
+
+
+draw_grid_for_grafic(cell_size, start_point_polygon)
+plt.show()
